4252888b7eeb6a8033b72c34b756e56ddca0af69
Bradford Lamson-Scribner <brad.lamson@gmail.com>
2016-06-16 11:33:17 -0600

    add built migrations

--- migrations/20160616111706_comment.js
+++ migrations/20160616111706_comment.js
@@ -1,13 +1,13 @@
 
 exports.up = function(knex, Promise) {
-  return knex.schema.createTable('post', function(table) {
+  return knex.schema.createTable('comment', function(table) {
     table.increments();
-    table.string('title');
     table.text('content');
+    table.integer('post_id').references('post.id');
     table.integer('user_id').references('user.id');
   });
 };
 
 exports.down = function(knex, Promise) {
-  return knex.schema.dropTable('post');
+  return knex.schema.dropTable('comment');
 };
